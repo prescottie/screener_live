fac8c48cf8e48660c815390fe97dd523f4eecb42
prescottie <scott.hagan@aol.com>
2021-01-28 22:15:37 -0800

    update pool size to env var

--- config/prod.exs
+++ config/prod.exs
@@ -25,7 +25,7 @@ config :screener_live, ScreenerLive.Repo,
   url: System.get_env("DATABASE_URL"),
   ssl: true,
   # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
-  pool_size: 2
+  pool_size: System.get_env("POOL_SIZE") || 2
 
 # Do not print debug messages in production
 config :logger, level: :info
