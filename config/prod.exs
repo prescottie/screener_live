e75f4f148b669fc99eefdff57725adf2cb13369c
prescottie <scott.hagan@aol.com>
2021-02-01 22:50:57 -0800

    try to force remove port on prod urls

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ use Mix.Config
 config :screener_live, ScreenerLiveWeb.Endpoint,
   # Possibly not needed, but doesn't hurt
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com"],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: nil],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   server: true
 
