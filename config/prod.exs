b739391d0119a0fec3e9820703584f492d4d6aca
prescottie <scott.hagan@aol.com>
2021-02-02 21:31:05 -0800

    remove static manifest

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,6 @@ config :screener_live, ScreenerLiveWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443, scheme: "https"],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 config :screener_live, ScreenerLive.Repo,
